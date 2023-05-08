34fb7fc3002677e662ebbe34df37adf37a4454f5
isabelafdevito <isabelavito5@gmail.com>
2023-05-08 11:18:55 -0300

    Exemplo de sala

---
+++ Aula 11 - Java IO/src/Anotacoes.java
@@ -0,0 +1,27 @@
+public class Anotacoes {
+
+    //SOBRE A ARQUITETURA MVC:
+
+    /*
+
+    O Modelo (Model) é responsável por lidar com a lógica de negócios e manipulação de dados.
+    Ele representa os dados e as regras de negócio subjacentes a uma aplicação e é responsável
+    por armazenar, recuperar e manipular esses dados.
+
+    A Visão (View) é responsável por apresentar os dados do modelo em uma forma que seja
+    visualmente agradável e facilmente compreensível pelo usuário. Ele representa a interface do
+    usuário (UI) da aplicação e é responsável por exibir informações para o usuário, incluindo formulários,
+    botões, tabelas, gráficos e outros elementos visuais.
+
+    O Controlador (Controller) é responsável por receber entrada do usuário, manipular a interação
+    com o modelo e atualizar a visão de acordo. Ele atua como intermediário entre o modelo e a visão
+    e é responsável por interpretar as solicitações do usuário e determinar como o modelo e
+    a visão devem ser atualizados em resposta a essas solicitações.
+
+    Em resumo, o Modelo (Model) é responsável por armazenar e manipular dados,
+    a Visão (View) é responsável por exibir esses dados de maneira agradável ao usuário,
+    e o Controlador (Controller) é responsável por lidar com a interação do usuário e atualizar o Modelo
+    e a Visão conforme necessário.
+*/
+
+}
--- Aula 11 - Java IO/src/controller/Arquivo.java
+++ Aula 11 - Java IO/src/controller/Arquivo.java
@@ -17,10 +17,36 @@ public class Arquivo {
         // bloco try catch para caso tenha algo de errado na escrita do funcionario no arquivo.txt
         try{
 
+            /* le o fluxo de bytes do arquivo com o nome que foi passado
+            se o arquivo com esse nome nao existir, ele cria um com esse nome dentro
+            das dependencias do projeto.
+
+            Ja o append serve para: se for true, iremos apenas inserir os novos dados
+            recebidos no final do arquivo, se for false, ele sobescreve o arquivo.
+             */
+
             os = new FileOutputStream("Funcionario.txt", true);
+
+            /*
+            O outputStreamWrites recebe os caracteres digitados pelo usuário e os transforma em bytes
+            para serem enviados.
+             */
             osw = new OutputStreamWriter(os);
+
+            /*
+            o BufferedWriter recebe caracteres como entrada, armazena temporariamente
+            esses caracteres em um buffer e, quando o buffer estiver cheio ou quando for solicitado
+            escreve esses caracteres no fluxo de saída (osw).
+            O fluxo de saída (osw) é responsável por converter esses caracteres em bytes
+            usando uma codificação de caracteres específica e escrevê-los em um arquivo.
+
+            Ou seja, o bw recebe os caracteres, os envia para o osw que os converte em bytes
+            e os envia para o os, que é responsável por escrever bytes em um arquivo.
+             */
+
             bw = new BufferedWriter(osw);
 
+            // escrevendo os valores usando bw:
             bw.write("Funcionario");
             bw.newLine();
             bw.write(func.nome + "\n");
@@ -31,7 +57,7 @@ public class Arquivo {
             System.out.println(e);
 
         }finally {
-// bloco try catch para tentar fechar a entrada de dados
+        // bloco try catch para tentar fechar a entrada de dados
             try {
                 bw.close();
             }catch (Exception e){
@@ -54,20 +80,37 @@ public class Arquivo {
         // bloco try catch para tentar ler um arquivo
         try{
 
+            /*
+            o FileInputStream é responsável por ler os bytes de um arquivo
+            cujo nome foi passado como parametro
+             */
             is = new FileInputStream("Funcionario.txt");
+
+            /*
+            O InputStreamReader é responsável por converter
+            os bytes em caracteres
+             */
             isr = new InputStreamReader(is);
+
+            /*
+            O BufferedReader que é responsável por ler os caracteres
+            a partir do fluxo de entrada e armazená-los em um buffer temporário,
+            a fim de melhorar a eficiência da leitura dos dados.
+             */
             br = new BufferedReader(isr);
 
-            linhaLer = br.readLine();
+
+            linhaLer = br.readLine(); //le a primeira linha do arquivo para ver se tem funcionarios cadastrados
             while(linhaLer != null){
-                if(linhaLer.contains("Funcionario")){
+
+                if(linhaLer.contains("Funcionario")){ // se conter funcionario, iremos ler as informações
                     Funcionario f = new Funcionario();
-                    f.nome = br.readLine();
-                    f.idade = Integer.parseInt(br.readLine());
-                    f.cpf = br.readLine();
-                    funcionarios.add(f);
+                    f.nome = br.readLine(); // le o nome que é uma string
+                    f.idade = Integer.parseInt(br.readLine()); // le a idade que é um int
+                    f.cpf = br.readLine(); // le um cpf que é uma string
+                    funcionarios.add(f); // armazena as informações de funcionário em uma lista
                 }
-                linhaLer = br.readLine();
+                linhaLer = br.readLine(); // pula pra proxima linha
             }
 
         }catch (Exception e){
