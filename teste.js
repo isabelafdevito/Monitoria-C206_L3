220dfacba6eb7c7a53a22ac7029aa4e64d214bbf
Isabela de Vito <80789978+isabelafdevito@users.noreply.github.com>
2024-09-30 09:20:41 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-try {
-    const flag = fs.readFileSync('flag.txt', 'utf8');
-    console.log(`Flag: ${flag}`);
-} catch (err) {
-    console.error('Erro ao ler o arquivo flag.txt:', err);
-}
